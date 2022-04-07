4dad0179bd54b26d18d22c0b1300a68308204f15
Marcus Page-Jones <marcus@pagejones.local>
2022-04-07 21:40:39 +0100

    event handlers

---
+++ commands/info/rules.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "rules",
+  category: "info",
+  permissions: [],
+  devOnly: false,
+  run: async ({client, message, args}) => {
+    message.reply(
+      
+   `1. Don't spam
+    2. Don't spam
+    3. Don't spam
+    4. Don't spam`)
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
+// const generateImage = require("./generateImage")
+
+// const answers = ["When you actually get laid (never)", "Idk, ask your mom", "No idea, but don't ask Gamu", "At some point within the next century", "Alt + F4 to find out"]
 
 const client = new Discord.Client({
     intents: [
@@ -11,24 +13,45 @@ const client = new Discord.Client({
     ]
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+    client,
+    prefix: "n.",
+    owners: ["699394539164663819"]
+}
 
-client.on("messageCreate", (message) => {
-    if (message.content == "when server") {
-        message.reply("When you actually get some maidens")
-    }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-const welcomeChannelId = "960606307839246397"
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
 
-client.on("guildMemberAdd", async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to Terracraft!`,
-        files: [img]
-    })
-})
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+// client.on("ready", () => {
+//    console.log(`Logged in as ${client.user.tag}`)
+// })
+
+// client.on("messageCreate", (message) => {
+//    if (message.content == "when server") {
+//       let randomMessageIndex = Math.floor(Math.random() * answers.length)
+
+        
+//        message.reply(answers[randomMessageIndex])
+        
+//    }
+//})
+
+// const welcomeChannelId = "960606307839246397"
+
+// client.on("guildMemberAdd", async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to Terracraft!`,
+//         files: [img]
+//     })
+// })  
 
 client.login(process.env.TOKEN)
